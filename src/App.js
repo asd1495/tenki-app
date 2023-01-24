71b51e4147d2f8272d10b7dc83849d69384fe06e
asd1495 <armando.salcedo95@gmail.com>
2023-01-23 23:11:18 -0600

    configured unicons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-2xl font-bold'>Tenki | てんき</h1>
+      <UilReact />
     </div>
   );
 }
