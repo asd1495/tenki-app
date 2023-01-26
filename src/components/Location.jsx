57a2d2cd5669dd7fb8d95e8a6026ef161bea91fa
asd1495 <armando.salcedo95@gmail.com>
2023-01-25 20:58:35 -0600

    added location and temperature components

---
+++ src/components/Location.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Location() {
+  return (
+    <div>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                Wednesday, January 25 2023 | 7:20 PM            
+            </p>
+        </div>
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium'>
+                Puebla, México
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Location
---
+++ src/components/Temperature.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { UilArrowUp, UilArrowDown, UilTemperature, UilTear, UilWind, UilSun, UilSunset } from '@iconscout/react-unicons'
+
+function Temperature() {
+  return (
+    <div className=''>
+        <div className='flex items-center justify-center py-6 text-cyan-300'>
+            <p>Cloudy with a chance of meatballs</p>
+        </div>
+        <div className='flex flex-row items-center justify-between text-white py-3'>
+            <img 
+                src="https://cdn-icons-png.flaticon.com/512/169/169367.png" 
+                alt="" 
+                className='w-20' 
+            />
+            <p className='text-5xl'>34°</p>
+            <div className='flex flex-col space-y-2'>
+                <div className="flex font-light text-sm items-center justify-center">
+                    <UilTemperature size={18} className='mr-1' />
+                    Feels like: 
+                    <span className='font-medium ml-1'> 32°</span>
+                </div>
+                <div className="flex font-light text-sm items-center justify-center">
+                    <UilTear size={18} className='mr-1' />
+                    Humidity: 
+                    <span className='font-medium ml-1'>65%</span>
+                </div>
+                <div className="flex font-light text-sm items-center justify-center">
+                    <UilWind size={18} className='mr-1' />
+                    Wind: 
+                    <span className='font-medium ml-1'>11 km/h</span>
+                </div>
+            </div>
+        </div>
+        <div className='flex flex-row items-center justify-center space-x-2 text-white text-sm py-3'>
+            <UilSun />
+            <p className='font-light'>
+                Rise: <span className='font-medium ml-1'>06:45 AM</span>
+            </p>
+            <p className="font-light">|</p>
+            <UilSunset />
+            <p className='font-light'>
+                Set: <span className='font-medium ml-1'>07:45 PM</span>
+            </p>
+            <p className="font-light">|</p>
+            <UilArrowUp />
+            <p className='font-light'>
+                High: <span className='font-medium ml-1'>35°</span>
+            </p>
+            <p className="font-light">|</p>
+            <UilArrowDown />
+            <p className='font-light'>
+                Low: <span className='font-medium ml-1'>28°</span>
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Temperature
